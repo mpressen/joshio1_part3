42bc6927d229b10ca9cb11a6731cdb5da6baf92f
Maximilien Pressensé <maximilien.pressense@hey.com>
2024-02-09 10:39:38 +0100

    scaffold user and check if sqLite persists db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_09_081020) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_09_093823) do
   create_table "posts", force: :cascade do |t|
     t.string "author"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
